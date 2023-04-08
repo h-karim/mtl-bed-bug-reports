2ea4011d9de3d189b24e643679b4a4f1b6f64ba4
Karim H <karimh96@hotmail.com>
2023-04-08 03:25:25 -0400

    add pagination test

--- data_retriever/src/tests/test_retriever.py
+++ data_retriever/src/tests/test_retriever.py
@@ -1,12 +1,17 @@
-import pytest
+import requests_mock  # noqa
+from ..app.retriever import Retriever
 
 
 class TestRetriever:
 
-    def test_request_information(self):
-        # dummy_data = [{}]
-        # mock return of _get
-        # ensure pagination is returned completely
-        # assert return == full data
-        
-        pass
+    def test_get_paginated_data_with_request_information(self, requests_mock):  # noqa
+        test_url = "http://andnothingelsematters"
+        test_url_next = "http://andnothingelsematters2"
+        response_json_next = {"_links": {"next": test_url_next}}
+        response_json_final = {}
+        responses = [response_json_next, response_json_final]
+        requests_mock.get(test_url, json=response_json_next)
+        requests_mock.get(test_url_next, json=response_json_final)
+        retriever = Retriever(test_url)
+        for i, response in enumerate(retriever.request_information()):
+            assert response.json() == responses[i]
