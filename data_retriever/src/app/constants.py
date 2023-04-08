a7ac7067945f92601df6b5c2c9bab7ffb747ce45
Karim H <karimh96@hotmail.com>
2023-04-08 03:22:33 -0400

    add query

--- data_retriever/src/app/constants.py
+++ data_retriever/src/app/constants.py
@@ -1,3 +1,4 @@
 BASE_URL = "https://data.montreal.ca/api/3/action/datastore_search"
-RESOURCE_ID_PARAMETER = "resource_id"
+RESOURCE_ID_KEY = "resource_id"
 RESOURCE_ID_VALUE = "6173de60-c2da-4d63-bc75-0607cb8dcb74"
+QUERY_PARAMETERS = {RESOURCE_ID_KEY: RESOURCE_ID_VALUE}
