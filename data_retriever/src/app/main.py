a39f51dca9952dc1195cec40fe1e334ee3291125
Karim H <karimh96@hotmail.com>
2023-04-08 04:38:06 -0400

    add main

---
+++ data_retriever/src/app/main.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+from retriever import DataRetriever
+from constants import BASE_URL, QUERY_PARAMETERS, PAGINATION_KEY
+
+if __name__ == "__main__":
+    data_retriever = DataRetriever(BASE_URL, query_params=QUERY_PARAMETERS, pagination_key=PAGINATION_KEY)
+
+    for data in data_retriever.request_information():
+        pprint(data.json()["result"]["records"])
