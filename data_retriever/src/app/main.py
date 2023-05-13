378a57de166ce5268d9b95b2ef7f6d1902a5d63b
Karim H <karimh96@hotmail.com>
2023-05-13 00:07:09 -0400

    clean main

--- data_retriever/src/app/main.py
+++ data_retriever/src/app/main.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from retriever import DataRetriever
 from constants import BASE_URL, QUERY_PARAMETERS, PAGINATION_KEY
 
@@ -6,9 +5,5 @@ if __name__ == "__main__":
     data_retriever = DataRetriever(BASE_URL, query_params=QUERY_PARAMETERS, pagination_key=PAGINATION_KEY)
 
     for data in data_retriever.retrieve_information():
-        a = data
-        pprint(data.json()["result"]["_links"]["next"])
-
-        pprint(a.json())
-        pprint(a.json()["result"]["records"][0])
+        # implement parser
         break
